76542e0acf627e33f204c1d2ba1688bec9ed89bb
cafferti <saulkumven@gmail.com>
2023-08-24 20:34:27 +0100

    Dom manipulation with Api

--- Apitemplate.js
+++ Apitemplate.js
@@ -1,9 +1,11 @@
 
+
 const validatevalue = () => {
-   let userDetail = {
-      username: ``,
-      useremail: ``
-   }
+
+let userDetail = {
+    username: ``,
+    useremail: ``
+ }
    const Name = document.querySelector(`.name`).value;
    const Email = document.querySelector(`.email`).value;
    const   err = document.querySelector(`.err`);
@@ -13,8 +15,34 @@ const validatevalue = () => {
     }else{
         userDetail.username = Name;
         userDetail.useremail = Email;
+        err.innerHTML = ``;
     }
     return userDetail
   };
 
+  const PostData = async (data) => {
+    const response = await fetch(`https://httpbin.org/post`, {
+      method: `POST`,
+      headers: {
+        "Content-type": `application/json`,
+      },
+      body: JSON.stringify(data),
+    });
+    const jokedata = await response.json();
+    console.log(jokedata);
+  };
+
+  function submitForm(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
   
+    const userDetail = validatevalue();
+    PostData(userDetail);
+  }
+  
+
+
+
+  
+
+  
+
