2b6403afc1b81bc288f475472035f95c7fdbfa57
cafferti <saulkumven@gmail.com>
2023-08-25 15:54:23 +0100

    Dom manipulation with Api

--- Apitemplate.js
+++ Apitemplate.js
@@ -1,38 +1,60 @@
+const   err = document.querySelector(`.err`)
 
-
-const validatevalue = (event) => {
-  event.preventDefault()
+const validatevalue = () => {
 
 let userDetail = {
-    username: ``,
-    useremail: ``,
-    userphone: ``,
-    userpassword:``
+    name: ``,
+    email: ``,
+    phone: ``,
+    password:``
  }
    const Name = document.querySelector(`.name`).value;
    const Email = document.querySelector(`.email`).value;
    const Phone = document.querySelector(`.phone`).value;
-    const Password = document.querySelector(`.email`).value;
+    const Password = document.querySelector(`.password`).value;
    
-   const   err = document.querySelector(`.err`);
+  ;
 
-    if (Name === `` || Email === `` || Phone === `` || password === ``) {
+    if (Name === `` || Email === `` || Phone === `` || Password === ``) {
       err.innerHTML = `please fill out the form`;
     }else{
-        userDetail.username = Name;
-        userDetail.useremail = Email;
-        userDetail.userphone = Phone;
-        userDetail.userpassword = Password;
+        userDetail.name = Name;
+        userDetail.email = Email;
+        userDetail.phone = Phone;
+        userDetail.password = Password;
         err.innerHTML = ``;
-
+        console.log(userDetail)
         return userDetail
     }
     
   };
 
 
-  const uploadata = (data) =>{
-     const response = await fetch(``)
+  const uploadata = async (data) =>{
+     const response = await fetch(`https://amica.onrender.com/users`,{
+      method: `POST`,
+      headers: {
+        "Content-type": `application/json`,
+      },
+      body: JSON.stringify(data),
+     })
+
+     const postdata = await response.json(data);
+     console.log(postdata)
+     
+  }
+
+  submitform = async (event) => {
+  event.preventDefault()
+     const postdata =  validatevalue()
+     if(postdata != null ){
+     await uploadata(postdata)
+      err.innerHTML = `succesful`
+     }else{
+      err.innerHTML = `please fill in the required details or Try again later`
+     }
+
+    
   }
 
  
