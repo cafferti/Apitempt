81319fb859440d31920d15dd0edfe2706342dfae
cafferti <saulkumven@gmail.com>
2023-08-25 23:35:06 +0100

    completed amica user template

--- login.js
+++ login.js
@@ -13,9 +13,34 @@ const getinput = () =>{
  }else{
     userDetail.email = Email
     userDetail.Password = Password
-    console.log(userDetail);
+    
 
     return userDetail
  }
 
 }
+
+const validatuser = async (data) =>{
+  const response = await fetch(`https://amica.onrender.com/login`,{
+    method: `POST`,
+      headers: {
+        "Content-type": `application/json`,
+      },
+      body: JSON.stringify(data),
+  })
+
+   const userDetail = await response.json()
+   console.log(userDetail)
+  
+}
+
+ const userlogin = async (event) =>{
+    event.preventDefault
+    const userdata = await getinput()
+    if(userdata !== null){
+        validatuser(userdata)
+        console.log(userdata);
+    }else{
+        message.innerHTML = (`please input your enail or password`) 
+    }
+ }
